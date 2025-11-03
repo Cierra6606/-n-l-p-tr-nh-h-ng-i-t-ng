309a8af2ce181d510c2a84f8b6238c87d05f257e
MK2418 <tranminhkiet090@gmail.com>
2025-11-03 17:22:22 +0700

    Update Butviet.java

--- Đồ Án/Butviet.java
+++ Đồ Án/Butviet.java
@@ -1,45 +1,26 @@
-import java.util.Scanner;
-
+// File: Butviet.java
 public class Butviet extends Dodunghoctap {
-    private int KichCoNgoi;
-    private String LoaiBut;
+    public String loaibut;
+    public int kichcongoi;
 
-    public Butviet(String MSP, String Ten, int SL, int DonGia, String DVTinh, int KichCoNgoi, String LoaiBut) {
-        super(MSP, Ten, SL, DonGia, DVTinh);
-        this.KichCoNgoi = KichCoNgoi;
-        this.LoaiBut = LoaiBut;
-    }
-
-    public Butviet() {
-    }
-
-    @Override
-    public void Nhap() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap Ma San Pham: ");
-        MSP = sc.nextLine();
-        System.out.print("Nhap Ten San Pham: ");
-        Ten = sc.nextLine();
-        System.out.print("Nhap So Luong: ");
-        SL = sc.nextInt();
-        System.out.print("Nhap Don Gia: ");
-        DonGia = sc.nextInt();
-        sc.nextLine(); // bỏ dòng
-        System.out.print("Nhap Don Vi Tinh: ");
-        DVTinh = sc.nextLine();
-        System.out.print("Nhap Kich Co Ngoi: ");
-        KichCoNgoi = sc.nextInt();
-        sc.nextLine();
+    public Butviet(String maSP, String tenSP, int soLuong, int donGia,
+            String donViTinh, String maLoai, String maSX,
+            String loaibut, int kichcongoi) {
+        super(maSP, tenSP, soLuong, donGia, donViTinh, maLoai, maSX);
+        this.loaibut = loaibut;
+        this.kichcongoi = kichcongoi;
     }
 
     @Override
-    public void Xuat() {
-        System.out.println("MSP: " + MSP);
-        System.out.println("Ten: " + Ten);
-        System.out.println("So luong " + SL);
-        System.out.println("Đon gia:" + DonGia);
-        System.out.println("Đon vi tinh" + DVTinh);
-        System.out.println("Kich co ngoi " + KichCoNgoi);
-        System.out.println("Loai but: " + LoaiBut);
+    public void xuat() {
+        String ten = tenSP.length() > 15 ? tenSP.substring(0, 12) + "..." : tenSP;
+        System.out.printf("%-10s %-15s %8d %,12d %-8s %-8s %-8s %-12s %10d\n",
+                maSP, ten, soLuong, donGia, donViTinh, maLoai, maSX, loaibut, kichcongoi);
+    }
+
+    @Override
+    public String toFileString() {
+        return String.format("%s,%s,%d,%d,%s,%s,%s,%s,%d",
+                maSP, tenSP, soLuong, donGia, donViTinh, maLoai, maSX, loaibut, kichcongoi);
     }
 }
