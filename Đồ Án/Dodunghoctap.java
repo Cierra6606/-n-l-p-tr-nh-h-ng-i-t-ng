16707d17717799661dd25175f5762e4632af5e3a
MK2418 <tranminhkiet090@gmail.com>
2025-11-03 17:27:33 +0700

    Update Dodunghoctap.java

--- Đồ Án/Dodunghoctap.java
+++ Đồ Án/Dodunghoctap.java
@@ -1,64 +1,34 @@
 public abstract class Dodunghoctap {
-    protected String MSP;
-    protected String Ten;
-    protected int SL;
-    protected int DonGia;
-    protected String DVTinh;
+    public String maSP, tenSP, donViTinh, maLoai, maSX;
+    public int soLuong, donGia;
 
-    public Dodunghoctap(String MSP, String Ten, int SL, int DonGia, String DVTinh) {
-        this.MSP = MSP;
-        this.Ten = Ten;
-        this.SL = SL;
-        this.DonGia = DonGia;
-        this.DVTinh = DVTinh;
+    public Dodunghoctap(String maSP, String tenSP, int soLuong, int donGia,
+            String donViTinh, String maLoai, String maSX) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.donViTinh = donViTinh;
+        this.maLoai = maLoai;
+        this.maSX = maSX;
     }
 
-    public Dodunghoctap() {
-    }
+    public abstract void xuat();
+
+    public abstract String toFileString();
+}
 
     // Get
-    public String getMSP() {
-        return MSP;
-    }
-
-    public String getTen() {
-        return Ten;
-    }
-
-    public int getSL() {
-        return SL;
-    }
-
-    public int getDonGia() {
-        return DonGia;
-    }
-
-    public String getDVTinh() {
-        return DVTinh;
-    }
-
+    public String getMSP() {return MSP;}
+    public String getTen() {return Ten;}
+    public int getSL() {return SL;}
+    public int getDonGia() {return DonGia;}
+    public String getDVTinh() {return DVTinh;}
     // set
-    public void setMSP(String MSP) {
-        this.MSP = MSP;
-    }
-
-    public void setTen(String Ten) {
-        this.Ten = Ten;
-    }
-
-    public void setSL(int SL) {
-        this.SL = SL;
-    }
-
-    public void setDonGia(int DonGia) {
-        this.DonGia = DonGia;
-    }
-
-    public void setDVTinh(String DVTinh) {
-        this.DVTinh = DVTinh;
-    }
-
-    public abstract void Nhap();
-
-    public abstract void Xuat();
+    public void setMSP(String MSP) {this.MSP = MSP;}
+    public void setTen(String Ten) {this.Ten = Ten;}
+    public void setSL(int SL) {this.SL = SL;}
+    public void setDonGia(int DonGia) {this.DonGia = DonGia;}
+    public void setDVTinh(String DVTinh) {this.DVTinh = DVTinh;}
 }
+
