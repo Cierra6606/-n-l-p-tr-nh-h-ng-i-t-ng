28c0ef0ddccf976ce1358d819a553c558e386c2d
MK2418 <tranminhkiet090@gmail.com>
2025-11-08 10:40:34 +0700

    Update QLKH.java

--- Đồ Án/QLKH.java
+++ Đồ Án/QLKH.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
-public class QLKH {
+public class QLKH extends QLBH {
     Dskhachhang ds = new Dskhachhang();
     Scanner sc = new Scanner(System.in);
-    
-    public void menu() throws Exception {
+
+    @Override
+    public void menuChinh() {
         int chon;
         do {
-            System.out.println("\n=== MENU QUẢN LÝ KHÁCH HÀNG ===");
-            System.out.println("1. Nhập danh sách khách hàng");
-            System.out.println("2. Xuất danh sách khách hàng");
-            System.out.println("3. Thêm khách hàng");
-            System.out.println("4. Xóa khách hàng");
-            System.out.println("5. Sửa khách hàng");
-            System.out.println("6. Tìm kiếm khách hàng");
-            System.out.println("0. Thoát");
-            System.out.print("Chọn: ");
-            chon = sc.nextInt(); sc.nextLine();
+            System.out.println("\n=== MENU QUAN LY HOA DON ===");
+            System.out.println("1. Xuat danh sach hoa don");
+            System.out.println("2. Them hoa don moi");
+            System.out.println("3. Xoa hoa don");
+            System.out.println("4. Sua hoa don");
+            System.out.println("5. Tim hoa don");
+            System.out.println("6. Thong ke hoa don theo ma khach hang");
+            System.out.println("0. Thoat");
+            System.out.print("Chon: ");
+            chon = sc.nextInt();
+            sc.nextLine();
 
             switch (chon) {
-                case 1 -> ds.docFile("DsKH.txt");
-                case 2 -> ds.xuat();
-                case 3 -> ds.them();
-                case 4 -> ds.xoa();
-                case 5 -> ds.sua();
-                case 6 -> ds.timkiem();
-                case 0 -> System.out.println("Thoát quản lý khách hàng.");
-                default -> System.out.println("Chọn sai, vui lòng nhập lại!");
+                case 1 -> ds.xuat();
+                case 2 -> ds.them();
+                case 3 -> ds.xoa();
+                case 4 -> ds.sua();
+                case 5 -> ds.timkiem();
+                case 0 -> System.out.println("Thoat quan ly hoa don.");
+                default -> System.out.println("Chon sai, vui long nhap lai!");
             }
         } while (chon != 0);
     }
